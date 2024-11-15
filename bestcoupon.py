d141c921994f37456eed164349a207542dcbc1a4
VaddepallyMeghana <158823458+VaddepallyMeghana@users.noreply.github.com>
2024-11-15 13:51:28 +0530

    Create bestcoupon.py

---
+++ bestcoupon.py
@@ -0,0 +1,6 @@
+bill = int(input())
+discount1 = bill * 0.10
+amount_after_percent = bill - discount1
+amount_after_flat = bill - 100
+final_amount = min(amount_after_flat, amount_after_flat)
+print(int(final_amount))
