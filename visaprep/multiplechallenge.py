28fa902a602c8584785f9fec5d891839a5683f70
VaddepallyMeghana <158823458+VaddepallyMeghana@users.noreply.github.com>
2024-11-18 12:20:14 +0530

    Create multiplechallenge.py

---
+++ visaprep/multiplechallenge.py
@@ -0,0 +1,8 @@
+def count_multiples(n):
+    count_3 = n // 3
+    count_5 = n // 5
+    count_15 = n // 15
+    total_count = count_3 + count_5 - count_15
+    return total_count
+n = int(input())
+print(count_multiples(n))
